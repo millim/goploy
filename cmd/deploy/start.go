54987b6a6fbf4b8969086465d6d762dd5cfc6bc7
millim <lidian@yiqijiao.cn>
2019-12-16 17:59:26 +0800

    update start

--- cmd/deploy/start.go
+++ cmd/deploy/start.go
@@ -9,12 +9,19 @@ import (
 //Start run file
 func Start() {
 	setting()
-	env := strings.Join(serverConfig.Env, " ")
-	cmd := fmt.Sprintf("%s %s", env, runCmd())
-	err := session.ExecCmd(cmd)
+	pro := fmt.Sprintf("ls /proc/`cat %s`", serverConfig.PidFile)
+	_, err := session.ExecCmdResponse(pro)
 	if err != nil {
-		fmt.Println(err)
-		return
+		env := strings.Join(serverConfig.Env, " ")
+		cmd := fmt.Sprintf("%s %s", env, runCmd())
+		err := session.ExecCmd(cmd)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("file start done!")
+	} else {
+		fmt.Println("Proc is run!")
 	}
-	fmt.Println("file start done!")
+
 }
